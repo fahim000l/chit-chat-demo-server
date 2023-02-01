1f35e6cd04cd154c5a8aede73ad97883ddb8e3fe
Fahim Faisal <mdfahimfaisal000@gmail.com>
2023-02-01 16:36:24 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const http = require("http");
+const { Server } = require("socket.io");
+const { Socket } = require("dgram");
+
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log(`user Connected :${socket.id}`);
+
+  socket.on("join_room", (data) => {
+    socket.join(data);
+    console.log(`user logedIn with Id :${socket.id} in room:${data}`);
+  });
+  socket.on("send_message", (data) => {
+    // console.log(data);
+    socket.to(data.room).emit("receive_message", data);
+  });
+});
+
+server.listen(port, () => {
+  console.log("Server is running on port :", port);
+});
